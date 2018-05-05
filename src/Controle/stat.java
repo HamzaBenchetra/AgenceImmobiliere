013d471faf40ab9e105660a9ae0335b948e235d4
CharafAbrit <charaf.abrit@univ-constantine2.dz>
2018-05-05 22:49:30 +0200

    jj
    j

--- src/Controle/stat.java
+++ src/Controle/stat.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class StattypeAppart
+ * Servlet implementation class stat
  */
-@WebServlet("/StattypeAppart")
-public class StattypeAppart extends HttpServlet {
+@WebServlet("/stat")
+public class stat extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public StattypeAppart() {
+    public stat() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,12 +35,7 @@ public class StattypeAppart extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String type=request.getParameter("type");
-		String statap=OperationsRESP.StatAppart(type);
-		request.setAttribute("statap",statap);
-		
-		this.getServletContext().getRequestDispatcher("/StattypeAppart2.jsp").forward(request, response);
-		
+		doGet(request, response);
 	}
 
 }
